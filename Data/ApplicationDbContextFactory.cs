cfdfdb61fb1581702ab209c11e21ddcbb47a35f8
AlaaFayyad <cr905143@gmail.com>
2025-07-09 13:31:35 +0300

    Fix connection string and EF Core DB context setup

--- Data/ApplicationDbContextFactory.cs
+++ Data/ApplicationDbContextFactory.cs
@@ -9,7 +9,7 @@ namespace MyPortfolio.Data
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            var connectionString = "server=dpg-d1mgrlidbo4c73fa3b60-a.oregon-postgres.render.com;port=5432;database=portfolio_db_hq2z;Username=root;password=Od009dlPoyRGsdMaBmvKbfonq4PgkFia;";
+            var connectionString = "Host=dpg-d1mgrlidbo4c73fa3b60-a.oregon-postgres.render.com;Port=5432;Database=portfolio_db_hq2z;Username=root;Password=Od009dlPoyRGsdMaBmvKbfonq4PgkFia;";
 
             optionsBuilder.UseNpgsql(connectionString);
 
